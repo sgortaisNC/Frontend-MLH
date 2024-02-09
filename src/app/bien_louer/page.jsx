1beaa2a5b8cd0f0b5fb81afdcc46b14361137bc0
Sébastien <sgortais@net-com.fr>
2024-02-09 13:57:42 +0100

    :sparkles: liste de biens

--- src/app/bien_louer/page.jsx
+++ src/app/bien_louer/page.jsx
@@ -8,7 +8,6 @@ import { useSearchParams } from 'next/navigation'
 
 
 export default function Page() {
-    const searchParams = useSearchParams()
     function handleForm(e) {
 
         e.preventDefault();
