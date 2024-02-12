94df0b6c803e745056fc333d8efe4b54378d7674
Sébastien <sgortais@net-com.fr>
2024-02-12 11:35:45 +0100

    :sparkles: Ajout Share

--- src/components/Titre/Titre.jsx
+++ src/components/Titre/Titre.jsx
@@ -1,10 +1,13 @@
 import './Titre.scss'
+import Share from "@/components/Share/Share";
 
 export default function Titre({titre, chapo}) {
     return <section className={'titre'}>
         <div className="container">
             <h1>{titre}</h1>
             {chapo && <div className="chapo" dangerouslySetInnerHTML={{__html: chapo}}></div>}
+            <Share />
         </div>
     </section>
+
 }
