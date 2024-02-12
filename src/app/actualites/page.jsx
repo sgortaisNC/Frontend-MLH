c16c6ee17cd3b421b6945a1029df602909919137
Sébastien <sgortais@net-com.fr>
2024-02-12 15:06:04 +0100

    :construction: WIP pagination - Liste actualité

--- src/app/actualites/page.jsx
+++ src/app/actualites/page.jsx
@@ -15,7 +15,7 @@ export default function Page() {
     if (!data) return <></>
 
 
-    const nbPerPage = 12;
+    const nbPerPage = 6;
     const pageMax = Math.ceil(data.posts.length / nbPerPage);
     const minNumber = Math.max(0, page - 2);
     const maxNumber = Math.min(pageMax, page + 3);
