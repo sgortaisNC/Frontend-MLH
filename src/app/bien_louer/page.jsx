19817a01f64cf8d7cb0f1bdad22e6be58ee57439
Sébastien <sgortais@net-com.fr>
2024-03-04 16:52:03 +0100

    :sparkles: Formulaire liste bien

--- src/app/bien_louer/page.jsx
+++ src/app/bien_louer/page.jsx
@@ -20,6 +20,7 @@ export default function Page() {
         setNombre(data.get('nombre'));
         setType(data.get('type'));
         setIsLoaded(false);
+        console.log('test')
         return false;
     }
 
@@ -31,8 +32,10 @@ export default function Page() {
 
     const [ville, setVille] = useState('');
     const [nombre, setNombre] = useState('');
+    const [surface, setSurface] = useState(250);
     const [type, setType] = useState('');
-    const [rayon, setRayon] = useState('');
+    const [rayon, setRayon] = useState(50);
+    const [loyer, setLoyer] = useState(1000);
 
     const [vueListe, setVueListe] = useState(true);
 
@@ -40,17 +43,14 @@ export default function Page() {
 
 
     useEffect(() => {
-        fetch(`https://api-montlucon.netcomdev2.com/wp-json/montlucon/v1/biens-louer?ville=${ville}&rayon=${rayon}&type=${type}&nombre=${nombre}`).then(r => {
+        console.log('useEffect');
+        fetch(`https://api-montlucon.netcomdev2.com/wp-json/montlucon/v1/biens-louer?loyer=${loyer}&ville=${ville}&rayon=${rayon}&type=${type}&surface=${surface}&nombre=${nombre}`).then(r => {
             return r.json();
         }).then(r => {
             setBiens(r.louer);
             setFiltres(r.filtres);
             console.log(r.filtres)
             setDefaultFiltres({
-                type: type === "" ? null : {
-                    "value": type,
-                    "label": r.filtres.types.find(f => f.value === parseInt(type)).label
-                },
                 nombre: nombre === "" ? null : {
                     "value": nombre,
                     "label": r.filtres.nombre_piece.find(f => f.value === parseInt(nombre)).label
@@ -64,6 +64,10 @@ export default function Page() {
             setIsLoaded(true)
         });
     }, [isLoaded]);
+
+    function reset() {
+        window.location.reload()
+    }
 
 
     if (filtres === []) return <></>
@@ -79,43 +83,159 @@ export default function Page() {
 
     return <div>
 
-        <Titre titre={'Biens à louer'} ariane={[{label: 'Biens à louer', url:''}]}/>
-        <div className="container" style={{marginBottom: '50px'}}>
+        <Titre titre={'Biens à louer'} ariane={[{label: 'Biens à louer', url: ''}]}/>
+        <div className="container">
+            <h2><strong className={"text-primary"}>Filtrer</strong> <strong>les biens par :</strong></h2>
+            <div className="filtres" style={{marginBottom: '50px'}}>
+                <div className="vues">
+                    <button type={'button'} className={!vueListe ? "active" : ''} onClick={() => setVueListe(false)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="21.375" height="16.626"
+                             viewBox="0 0 21.375 16.626">
+                            <path
+                                d="M6.312-15.4a.577.577,0,0,1,.382-.019L14.81-13.1l5.344-2.137a.89.89,0,0,1,1.221.827V-1.982a.9.9,0,0,1-.56.828l-5.752,2.3a.587.587,0,0,1-.382.019L6.565-1.154,1.221.983A.891.891,0,0,1,0,.156V-12.268a.891.891,0,0,1,.56-.826l5.752-2.3ZM1.188-.282l4.75-1.9V-13.967l-4.75,1.9ZM14.25-12.02,7.125-14.057V-2.23L14.25-.193ZM15.438-.282l4.75-1.9V-13.967l-4.75,1.9Z"
+                                transform="translate(0 15.438)" fill="#fff"/>
+                        </svg>
+                        Carte
+                    </button>
+                    <button type={'button'} className={vueListe ? "active" : ''} onClick={() => setVueListe(true)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18.406" height="15.438"
+                             viewBox="0 0 18.406 15.438">
+                            <path
+                                d="M.594-13.953a.891.891,0,0,1,.891-.891H3.266a.89.89,0,0,1,.891.891v1.781a.888.888,0,0,1-.891.891H1.484a.89.89,0,0,1-.891-.891Zm2.375,1.484v-1.187H1.781v1.188Zm15.438-1.187a.6.6,0,0,1,.594.594.6.6,0,0,1-.594.594H6.531a.6.6,0,0,1-.594-.594.6.6,0,0,1,.594-.594Zm0,5.938A.6.6,0,0,1,19-7.125a.6.6,0,0,1-.594.594H6.531a.6.6,0,0,1-.594-.594.6.6,0,0,1,.594-.594Zm0,5.938A.6.6,0,0,1,19-1.187a.6.6,0,0,1-.594.594H6.531a.6.6,0,0,1-.594-.594.6.6,0,0,1,.594-.594ZM3.266-8.906a.888.888,0,0,1,.891.891v1.781a.888.888,0,0,1-.891.891H1.484a.89.89,0,0,1-.891-.891V-8.016a.89.89,0,0,1,.891-.891ZM1.781-7.719v1.188H2.969V-7.719ZM.594-2.078a.89.89,0,0,1,.891-.891H3.266a.888.888,0,0,1,.891.891V-.3a.888.888,0,0,1-.891.891H1.484A.89.89,0,0,1,.594-.3ZM2.969-.594V-1.781H1.781V-.594Z"
+                                transform="translate(-0.594 14.844)" fill="#fff"/>
+                        </svg>
+                        Liste
+                    </button>
+                </div>
 
-            <button type={'button'} onClick={() => setVueListe(false)}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="21.375" height="16.626" viewBox="0 0 21.375 16.626">
-                    <path id="Tracé_15272" data-name="Tracé 15272"
-                          d="M6.312-15.4a.577.577,0,0,1,.382-.019L14.81-13.1l5.344-2.137a.89.89,0,0,1,1.221.827V-1.982a.9.9,0,0,1-.56.828l-5.752,2.3a.587.587,0,0,1-.382.019L6.565-1.154,1.221.983A.891.891,0,0,1,0,.156V-12.268a.891.891,0,0,1,.56-.826l5.752-2.3ZM1.188-.282l4.75-1.9V-13.967l-4.75,1.9ZM14.25-12.02,7.125-14.057V-2.23L14.25-.193ZM15.438-.282l4.75-1.9V-13.967l-4.75,1.9Z"
-                          transform="translate(0 15.438)" fill="#fff"/>
-                </svg>
-                Carte
-            </button>
-            <button type={'button'} onClick={() => setVueListe(true)}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="18.406" height="15.438" viewBox="0 0 18.406 15.438">
-                    <path id="Tracé_15273" data-name="Tracé 15273"
-                          d="M.594-13.953a.891.891,0,0,1,.891-.891H3.266a.89.89,0,0,1,.891.891v1.781a.888.888,0,0,1-.891.891H1.484a.89.89,0,0,1-.891-.891Zm2.375,1.484v-1.187H1.781v1.188Zm15.438-1.187a.6.6,0,0,1,.594.594.6.6,0,0,1-.594.594H6.531a.6.6,0,0,1-.594-.594.6.6,0,0,1,.594-.594Zm0,5.938A.6.6,0,0,1,19-7.125a.6.6,0,0,1-.594.594H6.531a.6.6,0,0,1-.594-.594.6.6,0,0,1,.594-.594Zm0,5.938A.6.6,0,0,1,19-1.187a.6.6,0,0,1-.594.594H6.531a.6.6,0,0,1-.594-.594.6.6,0,0,1,.594-.594ZM3.266-8.906a.888.888,0,0,1,.891.891v1.781a.888.888,0,0,1-.891.891H1.484a.89.89,0,0,1-.891-.891V-8.016a.89.89,0,0,1,.891-.891ZM1.781-7.719v1.188H2.969V-7.719ZM.594-2.078a.89.89,0,0,1,.891-.891H3.266a.888.888,0,0,1,.891.891V-.3a.888.888,0,0,1-.891.891H1.484A.89.89,0,0,1,.594-.3ZM2.969-.594V-1.781H1.781V-.594Z"
-                          transform="translate(-0.594 14.844)" fill="#fff"/>
-                </svg>
-                Liste
-            </button>
+                <form onSubmit={handleForm}>
+                    <div className="gridBiens">
+                        <div className="selecteur">
+                            <label htmlFor="">Communes</label>
+                            <Select
+                                className={'mySelect'}
+                                unstyled={true}
+                                classNamePrefix={"mySelect"}
+                                name="ville" defaultValue={defaultFiltres.ville} options={filtres.villes}
+                                isClearable={true}
+                                isSearchable={true}
+                                placeholder="Sélectionner"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="">Rayon : {rayon}km</label>
+                            <div className="range">
+                                <span>0km</span>
+                                <input
+                                    name={'rayon'}
+                                    type="range"
+                                    max={50}
+                                    min={0}
+                                    value={rayon}
+                                    onChange={(e) => setRayon(e.target.value)}
+                                />
+                                <span>50km</span>
+                            </div>
 
+                        </div>
 
-            <h2><strong>Filtrer</strong> les biens par :</h2>
-            <form onSubmit={handleForm}>
-                <Select name="type" defaultValue={defaultFiltres.type} options={filtres.types} isClearable={true}
-                        isSearchable={true}
-                        placeholder="Choisissez un filtre"/>
-                <Select name="nombre" defaultValue={defaultFiltres.nombre} options={filtres.nombre_piece}
-                        isClearable={true} isSearchable={true}
-                        placeholder="Choisissez un filtre"/>
-                <Select name="ville" defaultValue={defaultFiltres.ville} options={filtres.villes} isClearable={true}
-                        isSearchable={true}
-                        placeholder="Choisissez un filtre"/>
-                <button type={"submit"} className={"btn"}>Recherche</button>
-            </form>
+                        <div className="selecteur">
+                            <label htmlFor="">Type de biens</label>
+                            <div className="checkboxes">
+                                <label className={'checkLab'}>
+                                    <input
+                                        type="checkbox"
+                                        name={"type"}
+                                        value={'7'}
+                                        onChange={(e) => {
+                                            setType(e.target.value)
+                                        }}
+                                        {...type === '7' ? {checked: true} : {checked: false}}
+                                    /> Appartement
+                                </label>
+                                <label className={'checkLab'}>
+                                    <input type="checkbox"
+                                           name={"type"}
+                                           onChange={(e) => {
+                                               setType(e.target.value)
+                                           }}
+                                           value={'8'}
+                                           {...type === '8' ? {checked: true} : {checked: false}}
+                                    /> Maison
+                                </label>
+                            </div>
+                        </div>
+
+                        <div className="selecteur">
+                            <label htmlFor="">Nombre de pièces</label>
+                            <Select
+                                className={'mySelect'}
+                                unstyled={true}
+                                classNamePrefix={"mySelect"}
+                                name="nombre" defaultValue={defaultFiltres.nombre} options={filtres.nombre_piece}
+                                isClearable={true} isSearchable={true}
+                                placeholder="Choisissez un filtre"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="">Surface : {surface}m²</label>
+                            <div className="range">
+                                <span>10m²</span>
+                                <input
+                                    type="range"
+                                    name={"surface"}
+                                    max={250}
+                                    min={10}
+                                    step={10}
+                                    value={surface}
+                                    onChange={(e) => setSurface(e.target.value)}
+                                />
+                                <span>250m²</span>
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="">Loyer : {loyer}€/mois</label>
+                            <div className="range">
+                                <span>50€</span>
+                                <input
+                                    type="range"
+                                    name={"Loyer"}
+                                    step={50}
+                                    max={1000}
+                                    value={loyer}
+                                    onChange={(e) => setLoyer(e.target.value)}
+                                />
+                                <span>1000€</span>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="bottomAction">
+                        <button type={"submit"} className={"btn"}>
+                            Recherche
+                            <svg xmlns="http://www.w3.org/2000/svg" width="15.751" height="13.5"
+                                 viewBox="0 0 15.751 13.5">
+                                <path
+                                    d="M1.427-13.267,7.615-7.361a.844.844,0,0,1,.261.611.845.845,0,0,1-.261.611L1.427-.232A.845.845,0,0,1,.234-.261.843.843,0,0,1,.263-1.454L4.928-5.9H-7.031a.845.845,0,0,1-.844-.844.844.844,0,0,1,.844-.843H4.926L.261-12.043a.847.847,0,0,1-.028-1.195A.846.846,0,0,1,1.427-13.267Z"
+                                    transform="translate(7.875 13.5)" fill="currentColor"/>
+                            </svg>
+
+                        </button>
+                        <button className={"reset"} onClick={() => {
+                            reset()
+                        }}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16.879" height="16.875"
+                                 viewBox="0 0 16.879 16.875">
+                                <path
+                                    d="M6.986-5.621a.841.841,0,0,0-1.046.573A6.213,6.213,0,0,1-.032-.562a6.221,6.221,0,0,1-5.5-3.375H-2a.844.844,0,0,0,.844-.844.816.816,0,0,0-.812-.844H-7.6a.844.844,0,0,0-.844.844V.844a.844.844,0,0,0,.844.844A.844.844,0,0,0-6.754.844V-2.746A7.9,7.9,0,0,0-.032,1.125,7.915,7.915,0,0,0,7.53-4.546.836.836,0,0,0,6.986-5.621Zm.608-9.566a.845.845,0,0,0-.844.844v3.589A7.9,7.9,0,0,0,0-14.625,7.912,7.912,0,0,0-7.562-8.954.841.841,0,0,0-6.988-7.91a.84.84,0,0,0,1.046-.573A6.219,6.219,0,0,1,0-12.937,6.221,6.221,0,0,1,5.5-9.562H1.969a.846.846,0,0,0-.844.844.846.846,0,0,0,.844.844H7.594a.844.844,0,0,0,.844-.844v-5.625A.843.843,0,0,0,7.594-15.187Z"
+                                    transform="translate(8.441 15.188)" fill="#4b7388"/>
+                            </svg>
+                            Réinitialiser
+                        </button>
+                    </div>
+                </form>
+            </div>
 
             {vueListe ? <>
-                {biens.length === 0 && <p>Aucun bien ne correspond à votre recherche</p>}
+                {biens.length === 0 && <h2>Aucun bien ne correspond à votre recherche</h2>}
                 <div className="biens_grid">
                     {louer.map(bien => <TeaserLogement key={bien.id} bien={bien}/>)}
                 </div>
