c67376b2a480c0b03959cf1dded5c916cb1060d6
Sébastien <sgortais@net-com.fr>
2024-03-21 15:35:46 +0100

    :sparkles: SEO Single Offre

--- src/app/offre_emploi/[slug]/page.jsx
+++ src/app/offre_emploi/[slug]/page.jsx
@@ -1,23 +1,59 @@
-"use client";
-
 import Titre from "@/components/Titre/Titre";
-import useSWR from "swr";
 import Link from "next/link";
 import PostNotFound from "@/app/not-found";
 import './offre.scss';
 
-const fetcher = url => fetch(url).then(r => r.json());
 
-export default function Page({params}) {
+function strip(html)
+{
+    return html.replace(/<[^>]+>/ig,"").replace(/\s+/g, ' ').trim().substring(0, 160)
+}
+async function getData($slug) {
+    const res = await fetch(`https://api-montlucon.netcomdev2.com/wp-json/montlucon/v1/offre/${$slug}`)
 
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
 
-    const {
-        data,
-        error
-    } = useSWR(`https://api-montlucon.netcomdev2.com/wp-json/montlucon/v1/offre/${params.slug}`, fetcher)
+    return res.json()
+}
 
-    if (error) return <PostNotFound/>
-    if (!data) return <></>
+export async function generateMetadata({ params, searchParams }, parent) {
+    const lastSlug = params.slug;
+    const data = (await getData(lastSlug))[0];
+    let metas = {
+        title: data.titre,
+        openGraph: {
+            title: data.titre,
+            images: [
+                {
+                    url: data.image, // Must be an absolute URL
+                    width: 800,
+                    height: 600,
+                },
+            ],
+            locale: 'fr_FR',
+            type: 'website',
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title: data.titre,
+            images: [data.image], // Must be an absolute URL
+        },
+    }
+    if (data.chapo || data.contenu) {
+        metas.description = data.chapo ?? strip(data.contenu)
+        metas.openGraph.description = data.chapo ?? strip(data.contenu)
+        metas.twitter.description = data.chapo ?? strip(data.contenu)
+    }
+    return metas
+}
+
+export default async function Page({params}) {
+
+    const data = await getData(params.slug)
+
+    if (!data) return <PostNotFound/>
 
     const offre = data[0];
 
