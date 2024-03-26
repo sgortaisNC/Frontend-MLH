dcc0c9ea51d43d0daca8d7bf7ac351d4f7eccb26
Sébastien <sgortais@net-com.fr>
2024-03-26 15:16:03 +0100

    :sparkles: Plan du site

---
+++ src/app/plan-du-site/page.jsx
@@ -0,0 +1,50 @@
+import Titre from "@/components/Titre/Titre";
+
+async function getData() {
+    const res = await fetch('https://api-montlucon.netcomdev2.com/wp-json/montlucon/v1/options/header');
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+
+    return await res.json();
+}
+
+export default async function Page() {
+    const data = await getData();
+    console.log(data.menu);
+    return (
+        <div>
+            <Titre titre={"Plan du site"} chapo={data.menu.length}/>
+            <div className="container">
+                <div className="wysiwyg">
+                    <h2>Menu principal</h2>
+                    <ul className={"sitemap"}>
+                        {data.menu.map((item, index) => (
+                            <li key={index}>
+                                <a href={item.url}>{item.title}</a>
+                                {item.niveau2 && (
+                                    <ul>
+                                        {item.niveau2.map((child, indexB) => (
+                                            <li key={indexB}>
+                                                <a href={child.url}>{child.title}</a>
+                                                {child.niveau3 && (
+                                                    <ul>
+                                                        {child.niveau3.map((childB, indexC) => (
+                                                            <li key={indexC}>
+                                                                <a href={childB.url}>{childB.title}</a>
+                                                            </li>
+                                                        ))}
+                                                    </ul>
+                                                )}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                )}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
