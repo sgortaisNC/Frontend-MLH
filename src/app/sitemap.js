545556e8f7a70dc8ef41dbf1e8dd967c1b5955e0
Sébastien <sgortais@net-com.fr>
2024-03-26 14:38:42 +0100

    :sparkles: Sitemap XML - Clean code

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,6 +1,7 @@
 export default async function sitemap() {
 
-    const data = await fetch('https://api-montlucon.netcomdev2.com/wp-json/montlucon/v1/sitemap');
+    const data = await fetch('https://api-montlucon.netcomdev2.com/wp-json/montlucon/v1/sitemap',
+        {next: {revalidate: false}});
     const result = await data.json();
 
     return result;
