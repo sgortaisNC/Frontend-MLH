2434994658149a54341eff83b8d99e19ff9f51b1
Sébastien <sgortais@net-com.fr>
2024-05-27 10:22:49 +0200

    :bug: re7 > Wording Home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -90,7 +90,7 @@ export default async function Home() {
             </section>
             <div className="container">
                 <section className="biens">
-                    <h2><strong>Disponibles</strong> à la location</h2>
+                    <h2><strong>La sélection</strong> du moment</h2>
                     {data.biens.map((data) => <TeaserLogement key={data.id} bien={data}/>)}
                     <div className="biens__cta">
                         <Link href="/bien_louer" className="btn">Tous les logements à louer</Link>
