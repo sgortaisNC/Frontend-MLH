4cd37dd0626bbb922652221bf9c1f312fbf5b07f
Sébastien <sgortais@net-com.fr>
2024-06-11 16:40:55 +0200

    :bug: Attente pour sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,4 +1,5 @@
 export default async function sitemap() {
+    return [];
     const data = await fetch(`https://api.montlucon-habitat.fr/wp-json/montlucon/v1/sitemap`,
         {next: {revalidate: 0}});
     const result = await data.json();
