224519bde85d2f103a2b3275251cdb33114134c7
Sébastien <sgortais@net-com.fr>
2024-06-14 11:44:08 +0200

    :sparkles: Robots.txt

--- src/app/robots.js
+++ src/app/robots.js
@@ -5,6 +5,6 @@ export default function robots() {
             allow: '/',
             disallow: '/montlucon-habitat/espace-administrateur/',
         },
-        sitemap: 'https://montlucon-habitat.fr/sitemap.xml',
+        sitemap: 'https://www.montlucon-habitat.fr/sitemap.xml',
     }
 }
