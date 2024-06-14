261ccc0348ae3e8d932153c3167771d8c5226fe7
Sébastien <sgortais@net-com.fr>
2024-06-14 11:24:24 +0200

    :sparkles: Sitemap XML

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,15 @@ const nextConfig = {
     env:{
         NEXT_PUBLIC_BACK_DNS: process.env.NEXT_PUBLIC_BACK_DNS,
         NEXT_PUBLIC_FRONT_DNS: process.env.NEXT_PUBLIC_FRONT_DNS
+    },
+    redirects: async () => {
+        return [
+            {
+                source: '/123456',
+                destination: '/',
+                permanent: true,
+            },
+        ]
     }
 };
 
--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,5 +1,4 @@
 export default async function sitemap() {
-    return [];
     const data = await fetch(`https://api.montlucon-habitat.fr/wp-json/montlucon/v1/sitemap`,
         {next: {revalidate: 0}});
     const result = await data.json();
